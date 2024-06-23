24bace5eff0c41f6346ec160a0addf64bb417a0b
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2024-06-23 19:09:56 +0300

    initial

---
+++ README.md
@@ -0,0 +1,16 @@
+# Recycle tray 
+Adds the recycle bin icon to the system tray. <br>
+***For now it only supports Windows.*** <br>
+Technically it is **[recycle-tray](https://github.com/isc30/recycle-tray)**, but in ***🚀rust🚀***
+
+![img](./assets/prev.png)
+
+# How to use
+1. Download
+2. Open
+3. Profit 🚀🚀🚀
+
+# How to add to startup
+1. Win + R
+2. ```shell:startup```
+3. move executable to folder
---
+++ src/main.rs
@@ -0,0 +1,91 @@
+#![windows_subsystem = "windows"]
+
+use tray_icon::{
+    menu::{Menu, MenuEvent, MenuItemBuilder}, Icon, TrayIcon, TrayIconBuilder
+};
+use winapi::{shared::windef::HWND, um::shellapi::{SHERB_NOCONFIRMATION, SHERB_NOPROGRESSUI, SHERB_NOSOUND}};
+use winit::event_loop;
+
+fn get_icon() -> Icon {
+    let bytes = include_bytes!("icon.png");
+    let image = image::load_from_memory(bytes).unwrap();
+    let rgba = image.as_rgba8().unwrap();
+    Icon::from_rgba(rgba.to_vec(), image.width(), image.height()).unwrap()
+}
+
+fn main() {
+    let event_loop = event_loop::EventLoop::builder().build().unwrap();
+
+    let mut icon: Option<TrayIcon> = None;
+    let menu = Menu::new();
+    menu.append(
+        &MenuItemBuilder::new()
+            .enabled(true)
+            .id("open".into())
+            .text("Open")
+            .build(),
+    )
+    .unwrap();
+    menu.append(
+        &MenuItemBuilder::new()
+            .enabled(true)
+            .id("empty".into())
+            .text("Empty")
+            .build(),
+    )
+    .unwrap();
+    menu.append(
+        &MenuItemBuilder::new()
+            .enabled(true)
+            .id("exit".into())
+            .text("Exit")
+            .build(),
+    )
+    .unwrap();
+
+    let menu_channel = MenuEvent::receiver();
+
+    #[allow(deprecated)]
+    event_loop
+        .run(move |event, event_loop| {
+            event_loop.set_control_flow(event_loop::ControlFlow::WaitUntil(
+                std::time::Instant::now() + std::time::Duration::from_millis(16),
+            ));
+
+            if let winit::event::Event::NewEvents(winit::event::StartCause::Init) = event {
+                icon = Some(
+                    TrayIconBuilder::new()
+                        .with_menu(Box::new(menu.clone()))
+                        .with_title("Recycle Bin")
+                        .with_icon(get_icon())
+                        .build()
+                        .unwrap(),
+                )
+            }
+
+            if let Ok(event) = menu_channel.try_recv() {
+                if event.id == "exit" {
+                    icon.take();
+                    event_loop.exit();
+                }
+
+                if event.id == "open" {
+                    std::process::Command::new("explorer")
+                        .arg("shell:RecycleBinFolder")
+                        .spawn()
+                        .unwrap();
+                }
+
+                if event.id == "empty" {
+                    unsafe {
+                        winapi::um::shellapi::SHEmptyRecycleBinA(
+                            std::ptr::null_mut() as HWND,
+                            std::ptr::null(),
+                            SHERB_NOCONFIRMATION | SHERB_NOSOUND | SHERB_NOPROGRESSUI,
+                        );
+                    };
+                }
+            }
+        })
+        .unwrap();
+}
